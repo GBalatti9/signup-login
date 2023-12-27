2d7e1e47217dd7aa95235205e1be5da6d624df62
GBalatti9 <gas.balatti@gmail.com>
2023-12-27 11:19:08 -0300

    Handle error in case of empty fields and if the user already has an account

--- backend/controllers/signupControllers.js
+++ backend/controllers/signupControllers.js
@@ -1,17 +1,36 @@
+const { User } = require('../database/models');
 const { getDataForView } = require("../helpers");
 
+const viewData = getDataForView('register');
+viewData.title = 'Register';
 
 module.exports = {
 
-    getRegister: ( req, res ) => {
-
-        const viewData = getDataForView('register');
-        viewData.title = 'register'
-        
-        res.render('signup', {...viewData});
+    getRegister: ( req, res ) => {        
+        res.render('signup', { ...viewData });
     },
 
-    postRegister: ( req, res ) => {
+    postRegister: async ( req, res ) => {
+
+        const { firstName, lastName, email, password, checkPassword } = req.body;
+
+        if ( firstName === '' || lastName === '' || email === '' || password === '' || checkPassword === '' ) {
+            viewData.errors.message = 'Fields cannot be empty';
+            viewData.oldData = req.body;
+            return res.render('signup', { ...viewData })
+        }
+
+        try {
+            const user = await User.findOne({ where: { email: email } });
+
+            if ( user ) {
+                viewData.errors.message = 'You are already register, please login';
+                return res.render('signup', { ...viewData });
+            }
+
+        } catch (error) {
+            throw new Error( error )
+        }
 
         return res.json({ data: req.body })
 
