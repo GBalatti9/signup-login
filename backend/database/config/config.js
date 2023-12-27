e1de1016d216443b5363fb2591c8bef93c3a69a7
GBalatti9 <gas.balatti@gmail.com>
2023-12-27 11:05:06 -0300

    Setting configuration file for sequelize

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -2,9 +2,11 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "register-login-p",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "port": 3307,
+    "dialect": "mysql",
+    dialectModule: require('mysql2'),
   },
   "test": {
     "username": "root",
