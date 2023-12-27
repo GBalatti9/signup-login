f268173c1be22c93edb2ba4e65dbb0046bca2d57
GBalatti9 <gas.balatti@gmail.com>
2023-12-27 10:03:11 -0300

    App.set views routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', [
+    __dirname + './views',
+    __dirname + './views/auth'
+])
+
 const PORT = process.env.PORT || 3000;
 
 app.listen( PORT, () => {
