b35620d75d3eb78b0611c1a6864ce046a0a04ccd
GBalatti9 <gas.balatti@gmail.com>
2024-01-03 10:52:50 -0300

    Environment variables

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
     "username": "root",
@@ -16,10 +18,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port": process.env.PORT,
     "dialect": "mysql"
   }
 }
