c418baf2972410f361ced34f454ea6a0eed7def3
GBalatti9 <gas.balatti@gmail.com>
2024-01-02 23:17:14 -0300

    Init session with gmail working correctly. The user is saved en session

--- backend/controllers/gmailControllers.js
+++ backend/controllers/gmailControllers.js
@@ -3,14 +3,32 @@ const passport = require("passport");
 module.exports = {
 
     authenticate: ( req, res, next ) => {
+        console.log("AUTHENTICATE");
         passport.authenticate('google', { scope: [ 'email', 'profile' ] })( req, res, next );
     },
 
-    redirect: ( req, res, next ) => {
-        passport.authenticate('google', {
-            successRedirect: '/',
-            failureRedirect: '/auth/failure',
-        })( req, res, next )
+    redirect: async ( req, res, next ) => {
+
+        passport.authenticate('google', async( err, user ) => {
+
+            try {
+                if (err) {
+                    return next(err);
+                }
+
+                if (!user) {
+                    return res.redirect('/auth/failure');
+                }
+                
+                delete user.id;
+                delete user.password;
+                req.session.user = user;
+                return res.redirect('/');
+
+            } catch (error) {
+                return next(error);
+            }
+        })(req, res, next);
     },
 
     failureRedirect: ( req, res ) => {
