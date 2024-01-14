8bfc6167fb8c28d348f8ef1a4fefd10903ae2128
GBalatti9 <gas.balatti@gmail.com>
2024-01-14 20:38:30 -0300

    Cors policy in production

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use( express.urlencoded({ extended: true }) );
 app.use( express.json() );
 app.use( cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://argentina-world-cup.onrender.com'],
     credentials: true,
 }) );
 app.use( cookieParser() );
