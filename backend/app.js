43fc930a94a85fd25834777c584a8c5ed4dc06d4
GBalatti9 <gas.balatti@gmail.com>
2024-01-14 20:24:07 -0300

    Port for production

--- backend/app.js
+++ backend/app.js
@@ -65,6 +65,6 @@ app.set('views', [
 
 const PORT = process.env.PORT || 3000;
 
-app.listen( 3000, () => {
-    console.log(`Server running on ${ 3000 }`);
+app.listen( PORT, () => {
+    console.log(`Server running on ${ PORT }`);
 })
