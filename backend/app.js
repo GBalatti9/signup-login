3e71b3ecaaaafb609b3614e42a45ef741ae079a3
GBalatti9 <gas.balatti@gmail.com>
2024-01-19 13:03:04 -0300

    Adding another url to allow cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use( express.urlencoded({ extended: true }) );
 app.use( express.json() );
 app.use( cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://argentina-world-cup.onrender.com'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://argentina-world-cup.onrender.com', 'https://argentina-world-cup-7l3d.vercel.app/'],
     credentials: true,
 }) );
 app.use( cookieParser() );
